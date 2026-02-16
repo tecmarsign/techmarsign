f9f2112c86626fdf0be3c6896bbd5e0e64a11705
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2026-02-16 10:53:52 +0300

    update footer to use official email

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -25,7 +25,7 @@ const contactInfo = [
   {
     icon: Mail,
     title: "Email Us",
-    details: ["info@tecmarsign.academy", "admissions@tecmarsign.academy"],
+    details: ["info@tecmarsign.com", "admissions@tecmarsign.com", "support@tecmarsign.com"],
   },
 ];
 
